9612b930adb7bb3a8f8ebc930456b10f2281d04a
ayberkbgln <42419843+ayberkbgln@users.noreply.github.com>
2023-03-01 21:53:36 +0300

    commit

---
+++ ebyn/ebyn/bdp.bat
@@ -0,0 +1,2 @@
+rem 01.10.2005
+java -Xmx256m -Dcom.cs.uid.renderer.java.runtime.UIBeanClassCache.packageName= -Dcom.cs.uid.renderer.java.runtime.UIBeanClassCache.checkForModification=false -classpath beydef.jar;bdp.jar;csfc.jar;debugger.jar;cs_mdi.jar;lf.jar;javarenderer.jar;ice-5_06_3.jar bdp_mf
